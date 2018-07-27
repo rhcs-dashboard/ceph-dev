e07fa7351ec527b77cff88651bde5f20c462160e
alfonsomthd <almartin@redhat.com>
2018-07-27 12:25:18 +0200

    fix radosgw start (mailcap package needed); enable the Object Gateway management frontend

--- docker/ceph/start-ceph.sh
+++ docker/ceph/start-ceph.sh
@@ -7,3 +7,11 @@ cd /ceph/build
 rm -rf out dev
 
 RGW=1 ../src/vstart.sh -d -n
+
+# Enable the Object Gateway management frontend
+./bin/radosgw-admin user create --uid=dev --display-name=Dev --system
+./bin/ceph dashboard set-rgw-api-user-id dev
+readonly ACCESS_KEY=$(./bin/radosgw-admin user info --uid=dev | jq .keys[0].access_key | sed -e 's/^"//' -e 's/"$//')
+readonly SECRET_KEY=$(./bin/radosgw-admin user info --uid=dev | jq .keys[0].secret_key | sed -e 's/^"//' -e 's/"$//')
+./bin/ceph dashboard set-rgw-api-access-key "$ACCESS_KEY"
+./bin/ceph dashboard set-rgw-api-secret-key "$SECRET_KEY"
