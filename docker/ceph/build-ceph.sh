d6f3e56c0eb8a6ea058100ee1747748308fa997c
Ernesto Puerta <37327689+epuertat@users.noreply.github.com>
2018-09-26 18:11:29 +0200

    Update build-ceph.sh

--- docker/ceph/build-ceph.sh
+++ docker/ceph/build-ceph.sh
@@ -9,13 +9,13 @@ cd $CEPH_DIR
 
 rm -rf $CEPH_DIR/build
 
+$CEPH_DIR/do_cmake.sh
+
 # Tricks to improve CCACHE hit ratio
 export SOURCE_DATE_EPOCH=358228200
 export BUILD_DATE=$(date --utc --date=@${SOURCE_DATE_EPOCH} +%Y-%m-%d)
 export ARGS='-D ENABLE_GIT_VERSION=OFF'
 export CCACHE_SLOPPINESS="time_macros"
-
-$CEPH_DIR/do_cmake.sh
 
 rm -rf $CEPH_DIR/src/pybind/mgr/dashboard/frontend/node_modules
 
