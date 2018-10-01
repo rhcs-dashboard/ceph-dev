24e09eac5b4ac5466e4f7a5a2be596f48b82be36
alfonsomthd <almartin@redhat.com>
2018-10-01 19:11:10 +0200

    updated script to install docker on CentOS to also support RHEL

--- scripts/docker/install-docker-compose-centos-rhel.sh
+++ scripts/docker/install-docker-compose-centos-rhel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -e
 
 # Check user privileges:
@@ -13,6 +14,12 @@ yum remove docker* || true
 
 # Install the Docker CE dependencies.
 yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# if the system is RHEL, install required dependencies:
+readonly OS=$(cat /etc/system-release | awk '{print $1}')
+if [ "$OS" == 'Red' ]; then
+    yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.42-1.gitad8f0f7.el7.noarch.rpm
+fi
 
 # Docker provides a repository where you can fetch the stable Docker CE version. Install it with this command:
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
