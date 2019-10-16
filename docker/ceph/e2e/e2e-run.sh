a5adb3146e4045d404f1e95a406343a9a7f484ce
alfonsomthd <almartin@redhat.com>
2019-10-16 16:59:31 +0200

    updated e2e-run.sh

--- docker/ceph/e2e/e2e-run.sh
+++ docker/ceph/e2e/e2e-run.sh
@@ -6,7 +6,7 @@ cd /ceph/src/pybind/mgr/dashboard/frontend
 
 npm install
 
-ARGS="--dev-server-target --webdriverUpdate=false"
+ARGS="--dev-server-target"
 ANGULAR_VERSION=$(npm run ng version | grep 'Angular: ' | awk '{ print substr($2,1,1) }')
 # In nautilus this flag is required because BASE_URL is not read in protractor config.
 if [[ "$ANGULAR_VERSION" -le 7 ]]; then
