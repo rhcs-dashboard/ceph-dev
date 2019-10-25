e3a51678f517c273a228d0cadcb9d0ba0682ff2e
alfonsomthd <almartin@redhat.com>
2019-10-25 08:55:50 +0200

    move to MGR_PYTHON_PATH to set-start-env.sh (refactoring for podman)

--- docker/ceph/set-start-env.sh
+++ docker/ceph/set-start-env.sh
@@ -5,6 +5,7 @@ set -e
 [[ -z "$CEPH_VERSION" ]] && export CEPH_VERSION=$("$CEPH_BIN"/ceph -v | awk '{ print substr($3,1,2) }')
 [[ "$CEPH_VERSION" == 'De' ]] && export CEPH_VERSION=1000000
 [[ -z "$MGR" ]] && export MGR=1
+[[ -z "$MGR_PYTHON_PATH" ]] && export MGR_PYTHON_PATH=/ceph/src/pybind/mgr
 [[ -d "$MGR_PYTHON_PATH"/dashboard/frontend ]] && export IS_UPSTREAM_LUMINOUS=0
 [[ -z "$RGW" ]] && export RGW=1
 
