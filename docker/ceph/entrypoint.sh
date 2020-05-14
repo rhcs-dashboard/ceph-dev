4b7c1c5c415fd20d701bfa48e711fdda06a61c99
alfonsomthd <almartin@redhat.com>
2020-05-14 17:27:15 +0200

    Make entrypoint.sh exported vars available in interactive shell

--- docker/ceph/entrypoint.sh
+++ docker/ceph/entrypoint.sh
@@ -2,33 +2,36 @@
 
 set -e
 
-export IS_CEPH_RPM=$(which ceph-mgr 2>/dev/null | wc -l)
+export_var() {
+    export "$1"
+    echo "export $1" >> /root/.bashrc
+}
+
+export_var IS_CEPH_RPM=$(which ceph-mgr 2>/dev/null | wc -l)
 
 # Env. vars used in vstart.
-export CEPH_ASOK_DIR=/ceph/build."${HOSTNAME}"/out
-export CEPH_CONF=/ceph/build."${HOSTNAME}"/ceph.conf
-export CEPH_CONF_PATH=/ceph/build."${HOSTNAME}"
-export CEPH_DEV_DIR=/ceph/build."${HOSTNAME}"/dev
-export CEPH_OUT_DIR=/ceph/build."${HOSTNAME}"/out
-
-echo "export CEPH_CONF=${CEPH_CONF}" >> /root/.bashrc
+export_var CEPH_ASOK_DIR=/ceph/build."${HOSTNAME}"/out
+export_var CEPH_CONF=/ceph/build."${HOSTNAME}"/ceph.conf
+export_var CEPH_CONF_PATH=/ceph/build."${HOSTNAME}"
+export_var CEPH_DEV_DIR=/ceph/build."${HOSTNAME}"/dev
+export_var CEPH_OUT_DIR=/ceph/build."${HOSTNAME}"/out
 
 mkdir -p "${CEPH_CONF_PATH}"
 
 if [[ "${IS_CEPH_RPM}" == 1 ]]; then
-    export CEPH_BIN=/usr/bin
-    export CEPH_LIB=/usr/lib64/ceph
-    export EC_PATH="$CEPH_LIB"/erasure-code
-    export OBJCLASS_PATH=/usr/lib64/rados-classes
+    export_var CEPH_BIN=/usr/bin
+    export_var CEPH_LIB=/usr/lib64/ceph
+    export_var EC_PATH="$CEPH_LIB"/erasure-code
+    export_var OBJCLASS_PATH=/usr/lib64/rados-classes
     
     ln -sf "$EC_PATH"/* "$CEPH_LIB"
     ln -sf "$OBJCLASS_PATH"/* "$CEPH_LIB"
     
     if [[ -z "$CEPH_REPO_DIR" ]]; then
         # The dashboard is a separate noarch package since v14.1
-        export MGR_PYTHON_PATH=/usr/share/ceph/mgr
+        export_var MGR_PYTHON_PATH=/usr/share/ceph/mgr
         if [[ ! -d "$MGR_PYTHON_PATH" ]]; then
-            export MGR_PYTHON_PATH="$CEPH_LIB"/mgr
+            export_var MGR_PYTHON_PATH="$CEPH_LIB"/mgr
         fi
     fi
 
@@ -43,7 +46,7 @@ WITH_RBD:BOOL=ON
 ' > /ceph/build/CMakeCache.txt
 else
     # Ceph build mode.
-    export CEPH_BIN=/ceph/build/bin
+    export_var CEPH_BIN=/ceph/build/bin
 
     readonly BUILD_DIR=/ceph/build
     readonly CUSTOM_BUILD_DIR=/build
@@ -53,14 +56,14 @@ else
         mkdir -p $BUILD_DIR
         mount -o bind $CUSTOM_BUILD_DIR $BUILD_DIR
     
-        export CUSTOM_BUILD_DIR_ENABLED=1
+        export_var CUSTOM_BUILD_DIR_ENABLED=1
     fi
 fi
 
 # Common env. vars.
-[[ -z "$CEPH_VERSION" ]] && export CEPH_VERSION=$("$CEPH_BIN"/ceph -v | awk '{ print substr($3,1,2) }')
-[[ "$CEPH_VERSION" == 'De' ]] && export CEPH_VERSION=1000000
-[[ -z "$CEPH_PATCH_VERSION" ]] && export CEPH_PATCH_VERSION=$("$CEPH_BIN"/ceph -v | sed -r 's/.*\.([0-9]*)\-.*/\1/')
-[[ $(rpm -qi ceph-mgr-dashboard | grep 'Red Hat' | wc -l) > 0 ]] && export IS_UPSTREAM=0 || export IS_UPSTREAM=1
+[[ -z "$CEPH_VERSION" ]] && export_var CEPH_VERSION=$("$CEPH_BIN"/ceph -v | awk '{ print substr($3,1,2) }')
+[[ "$CEPH_VERSION" == 'De' ]] && export_var CEPH_VERSION=1000000
+[[ -z "$CEPH_PATCH_VERSION" ]] && export_var CEPH_PATCH_VERSION=$("$CEPH_BIN"/ceph -v | sed -r 's/.*\.([0-9]*)\-.*/\1/')
+[[ $(rpm -qi ceph-mgr-dashboard | grep 'Red Hat' | wc -l) > 0 ]] && export_var IS_UPSTREAM=0 || export_var IS_UPSTREAM=1
 
 exec "$@"
