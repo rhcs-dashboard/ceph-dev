a1de796f45d70c7d8f09bef73a4dabfbdfcc7730
Ernesto Puerta <37327689+epuertat@users.noreply.github.com>
2021-05-11 19:19:54 +0200

    Fix quote mismatch

--- docker/ceph/ci/commit-msg.sh
+++ docker/ceph/ci/commit-msg.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-echo "Running commit-msg hook...'
+echo "Running commit-msg hook..."
 
 readonly BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ "$BRANCH" == ceph-*-rhel-patches ]]; then
     grep "^Resolves: rhbz#[0-9]\+" "$1" || {
-        echo >&2 Missing 'Resolves: rhbz#<bz_number>' in commit message
+        echo >&2 "Missing 'Resolves: rhbz#<bz_number>' in commit message"
         exit 1
     }
 fi
