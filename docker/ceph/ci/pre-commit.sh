9a35d968375699b69b2dfd037c2073953c753fd7
Nizamudeen A <nia@redhat.com>
2022-03-22 17:17:54 +0530

    pre-commit: declare and assign separately to avoid masking return values
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- docker/ceph/ci/pre-commit.sh
+++ docker/ceph/ci/pre-commit.sh
@@ -4,23 +4,34 @@ set -e
 
 echo 'Running pre-commit hook...'
 
+# shellcheck disable=SC1091
 source /docker/ci/sanity-checks.sh
 
-readonly NPM_PACKAGE_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB | grep -E "package(-lock){0,1}.json" | wc -l)
-readonly HTML_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.html" | wc -l)
-readonly SCSS_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.scss" | tr '\n' ' ')
-readonly TS_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.ts" | tr '\n' ' ')
-readonly GHERKIN_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.feature" | tr '\n' ' ')
-readonly JEST_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.spec.ts" | grep -v "/e2e/" | tr '\n' ' ')
-readonly PY_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.py" | tr '\n' ' ')
-readonly DOC_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "doc/*.rst" | wc -l)
-readonly MONITORING_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "monitoring/grafana/dashboards/*" | wc -l)
+NPM_PACKAGE_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB | grep -E "package(-lock){0,1}.json" -c)
+HTML_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.html" | wc -l)
+SCSS_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.scss" | tr '\n' ' ')
+TS_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.ts" | tr '\n' ' ')
+GHERKIN_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.feature" | tr '\n' ' ')
+JEST_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.spec.ts" | grep -v "/e2e/" | tr '\n' ' ')
+PY_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "*.py" | tr '\n' ' ')
+DOC_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "doc/*.rst" | wc -l)
+MONITORING_FILES=$(git diff --cached --name-only --diff-filter=ACMRTUXB -- "monitoring/grafana/dashboards/*" | wc -l)
 
-if [[ "$NPM_PACKAGE_FILES" > 0 || -n "$SCSS_FILES" || -n "$TS_FILES" ]]; then
+readonly NPM_PACKAGE_FILES
+readonly HTML_FILES
+readonly SCSS_FILES
+readonly TS_FILES
+readonly GHERKIN_FILES
+readonly JEST_FILES
+readonly PY_FILES
+readonly DOC_FILES
+readonly MONITORING_FILES
+
+if [[ "$NPM_PACKAGE_FILES" -gt 0 || -n "$SCSS_FILES" || -n "$TS_FILES" ]]; then
     run_npm_ci
 fi
 
-if [[ "$HTML_FILES" > 0 && -z "$SCSS_FILES" && -z "$TS_FILES" ]]; then
+if [[ "$HTML_FILES" -gt 0 && -z "$SCSS_FILES" && -z "$TS_FILES" ]]; then
     run_npm_lint_html
 fi
 
@@ -36,10 +47,10 @@ if [[ -n "$SCSS_FILES" || -n "$TS_FILES" || -n "$GHERKIN_FILES" ]]; then
 fi
 
 if [[ -n "$JEST_FILES" ]]; then
-    run_jest ${JEST_FILES}
+    run_jest "${JEST_FILES}"
 fi
 
-if [[ "$HTML_FILES" > 0 || -n "$TS_FILES" ]]; then
+if [[ "$HTML_FILES" -gt 0 || -n "$TS_FILES" ]]; then
     run_npm_i18n \
         || (echo "FIXING: adding $TRANSLATION_FILE to commit..." \
         && cd "$REPO_DIR" \
@@ -51,11 +62,11 @@ if [[ -n "$PY_FILES" ]]; then
     run_mypy "$PY_FILES"
 fi
 
-if [[ "$MONITORING_FILES" > 0 ]]; then
+if [[ "$MONITORING_FILES" -gt 0 ]]; then
     run_monitoring
 fi
 
-if [[ "$DOC_FILES" > 0 ]]; then
+if [[ "$DOC_FILES" -gt 0 ]]; then
     run_build_doc
 fi
 
