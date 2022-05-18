a274dd78fcb27b4e25af093917f5fb4f95e87bb3
Seremba Patrick <pserembae@gmail.com>
2022-05-18 23:12:05 +0300

    cleaned up the script of commented code.
    Signed-off-by: Seremba Patrick <pserembae@gmail.com>

--- docker/scripts/install-docker-compose-ubuntu.sh
+++ docker/scripts/install-docker-compose-ubuntu.sh
@@ -8,17 +8,6 @@ if [ "$(whoami)" != 'root' ]; then
     exit 1
 fi
 
-# Install kernel extra's to enable docker aufs support
-# sudo apt-get -y install linux-image-extra-$(uname -r)
-
-# Add Docker PPA and install latest version
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-# sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-# sudo apt-get update
-# sudo apt-get install lxc-docker -y
-
-# Alternatively you can use the official docker install script
-# wget -qO- https://get.docker.com/ | sh
 
 # Install docker from ubuntu repository 
 sudo apt remove --yes docker docker-engine docker.io containerd runc || true
