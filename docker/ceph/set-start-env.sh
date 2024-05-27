cc7a58ea0045727a20b05c42e8237d73f3ccba7c
Nizamudeen A <nia@redhat.com>
2024-05-27 14:10:15 +0530

    support multisite in ceph2 setup
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- docker/ceph/set-start-env.sh
+++ docker/ceph/set-start-env.sh
@@ -7,21 +7,23 @@ set -e
 [[ -d "$MGR_PYTHON_PATH"/dashboard/frontend ]] && export IS_UPSTREAM_LUMINOUS=0
 [[ -z "$RGW" ]] && export RGW=1
 
-export IS_FIRST_CLUSTER=$(hostname | grep -v cluster | wc -l)
+echo "hostname $(hostname)"
+export IS_FIRST_CLUSTER=$(( $(hostname | grep -E '(-cluster2|ceph2)' | wc -l) == 0 ))
+echo "is first cluster $IS_FIRST_CLUSTER"
 
-[[ ("$IS_CEPH_RPM" == 0 || -n "$CEPH_REPO_DIR") && "$IS_UPSTREAM_LUMINOUS" == 0 && "$IS_FIRST_CLUSTER" == 1 ]] \
+[[ ("$IS_CEPH_RPM" == 0 || -n "$CEPH_REPO_DIR") && "$IS_UPSTREAM_LUMINOUS" == 0 ]] \
     && export FRONTEND_BUILD_REQUIRED=1
 FRONTEND_BUILD_OPTIONS=${FRONTEND_BUILD_OPTIONS:-"--deleteOutputPath=false"}
 
 if [[ "$RGW_MULTISITE" == 1 ]]; then
     export RGW=0  # Required to prevent vstart from starting any rgw daemon.
 
-    if [[ "$IS_FIRST_CLUSTER" == 0 ]]; then
-        export FS=0
-        export MDS=0
-        export MGR=0
-        export MON=1
-    fi
+    # if [[ "$IS_FIRST_CLUSTER" == 0 ]]; then
+    #     export FS=0
+    #     export MDS=0
+    #     export MGR=0
+    #     export MON=1
+    # fi
 fi
 
 RGW_DEBUG=''
